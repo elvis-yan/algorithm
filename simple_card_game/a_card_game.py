0ef315cd02106c5e0e5fe6b9e534e9be8532474c
elvis-yan <yandd0423@163.com>
2018-08-11 01:24:11 +0800

    it's simple and not robust, but you can play it now

--- simple_card_game/a_card_game.py
+++ simple_card_game/a_card_game.py
@@ -5,6 +5,8 @@
 
 
 import random
+import os
+import sys
 
 def memo(fn):
     cache = {}
@@ -52,7 +54,116 @@ def max_min_iteratively(cards):
     return T
     
 
-cards = gen_cards(60)
-print(cards)
-print(max_min(cards))
-print(max_min_iteratively(cards)[0][len(cards)-1])
+def reconstruct_path(cards, T):
+    path = []
+    i, j = 0, len(cards)-1
+    while i <= j:
+        if i == j:
+            path.append(0)
+            break
+        _, b = T[i+1][j]
+        if T[i][j][0] == b+cards[i]:
+            path.append(0) # head
+            i += 1
+        else:
+            path.append(1) # tail
+            j -= 1
+    return path
+
+def strategy(cards):
+    T = max_min_iteratively(cards)
+    i, j = 0, len(cards)-1
+    if i == j:
+        return 0
+    # i < j
+    _, b = T[i+1][j]
+    if T[i][j][0] == b + cards[i]:
+        return 0
+    else:
+        return 1
+
+def game(n=20):
+    cards = gen_cards(n)
+    cards2 = cards[:]
+    you = int(input('play as player1 or player2[1/2]? ')) # it's dangerous but simple
+
+    if you == 1:
+        player1 = Player('you  ', you_strategy)
+        player2 = Player('robot')
+    else:
+        player1 = Player('robot')
+        player2 = Player('you  ', you_strategy)
+    
+    while cards:
+        # print('\n\t\t', player1.name, player1.score)
+        # print('\t\t\n', player2.name, player2.score)
+        print(cards)
+        cards = player1.play(cards)
+        # print(cards)
+        cards = player2.play(cards)
+
+    print(cards2)
+    print(player1.name, player1.score, player1.actions)
+    print(player2.name, player2.score, player2.actions)
+
+
+class Player:
+    def __init__(self, name, strategy=strategy):
+        self.name = name
+        self.strategy = strategy
+        self.score = 0
+        self.actions = []
+    
+    def play(self, cards):
+        if len(cards) == 0:
+            return cards
+        action = self.strategy(cards)
+        i = action if action == 0 else -1
+        print('\t{} take {}'.format(self.name, cards[i]))
+        if action == 0:
+            self.score += cards[0]
+            self.actions.append(cards[0])
+            return cards[1:]
+        else:
+            self.score += cards[-1]
+            self.actions.append(cards[1])
+            return cards[:-1]
+
+def you_strategy(cards):
+    action = input('take which [head(0)/tail(1)]? ')
+    return int(action)
+
+
+def test_reconstruct_path():
+    for _ in range(100):
+        cards = gen_cards(60)
+        T = max_min_iteratively(cards)
+        path = reconstruct_path(cards, T)
+        player1 = [action for (i,action) in enumerate(path) if i%2==0]
+        player2 = [action for (i,action) in enumerate(path) if i%2==1]
+        s1, s2 = 0, 0
+        for i in range(len(player1)):
+            a1, a2 = player1[i], player2[i]
+            if a1 == 0:
+                s1 += cards[0]
+                cards = cards[1:]
+            else:
+                s1 += cards[-1]
+                cards = cards[:-1]
+            if a2 == 0:
+                s2 += cards[0]
+                cards = cards[1:]
+            else:
+                s2 += cards[-1]
+                cards = cards[:-1] 
+        assert T[0][-1] == (s1, s2)
+    print('reconstruct_path test pass.')
+        
+
+if __name__ == '__main__':
+    # test_reconstruct_path()
+    if len(sys.argv) > 1:
+        n = int(sys.argv[1])
+        game(n)
+    else:
+        game()
